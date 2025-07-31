8c10d99cbd6c40984433042d576952a81a60e51e
Ronald Labrador <ronlabdev@gmail.com>
2025-07-31 15:45:03 -0500

    create user entity and import in user module

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @Column({ length: 255 })
+  lastName: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ default: 'user' })
+  rol: string;
+
+  @Column()
+  createdAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
