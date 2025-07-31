5fb07def96662cae2402bbcfab7f357e52d1672e
Ronald Labrador <ronlabdev@gmail.com>
2025-07-31 15:10:17 -0500

    Improve ts config and server running message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Crypto Investment </br> Server running on port: ${process.env.PORT ?? 4000} 🚀`;
   }
 }
